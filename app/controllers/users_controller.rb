b7d4b989bcb26beb535c22690365de6afe4a95cd
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-03-07 23:59:30 +0900

    Update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
+# frozen_string_literal: true
+
+# This is UserController
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to :index, notice: "Create new user: #{@user.name} !"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
