6a4de6e3cd0bb30f6d88d7c34372b5abda93b691
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-03-07 23:59:52 +0900

    Add index.tsx

---
+++ app/javascript/packs/images/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return(
+    <div>
+      <h1> Test Index </h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("image"));
