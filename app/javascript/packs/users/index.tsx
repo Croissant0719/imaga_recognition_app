b4f95c1fb5fc43342d19b07d7471eb172c377590
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-03-14 23:00:24 +0900

    Set User index

---
+++ app/javascript/packs/users/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Index = () => {
+  return(
+    <div>
+      <h1> Users Index </h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("image"));
