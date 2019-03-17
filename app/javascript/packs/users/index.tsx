a808cb598a74f25848dc136194a14948609f1d38
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-03-18 00:09:19 +0900

    fix

--- app/javascript/packs/users/index.tsx
+++ app/javascript/packs/users/index.tsx
@@ -25,46 +25,66 @@ const Index = () => {
   return (
     <div>
       <h1> Users Index </h1>
-      <div>
-        <label>名前</label>
-        <input
-          name="name"
-          placeholder="username"
-          onBlur={e => {
-            setUserName(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          name="email"
-          placeholder="email"
-          onBlur={e => {
-            setEmail(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <label>password</label>
-        <input
-          type="password"
-          name="password"
-          onBlur={e => {
-            setUserPassword(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <label>passwordの確認</label>
-        <input
-          type="password"
-          name="password_confirmation"
-          onBlur={e => {
-            setUserPasswordConfirm(e.target.value);
-          }}
-        />
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <label>名前</label>
+            </td>
+            <td>
+              <input
+                name="name"
+                placeholder="username"
+                onBlur={e => {
+                  setUserName(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Email</label>
+            </td>
+            <td>
+              <input
+                name="email"
+                placeholder="email"
+                onBlur={e => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>password</label>
+            </td>
+            <td>
+              <input
+                type="password"
+                name="password"
+                onBlur={e => {
+                  setUserPassword(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>passwordの確認</label>
+            </td>
+            <td>
+              <input
+                type="password"
+                name="password_confirmation"
+                onBlur={e => {
+                  setUserPasswordConfirm(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <button type="submit" onClick={() => console.log(userInfo)}>
         決定
       </button>
