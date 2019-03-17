fff74a08ec7ac9db27ad91ffcd4f41d9b11bdfe6
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-03-18 00:01:03 +0900

    prettier

--- app/javascript/packs/users/index.tsx
+++ app/javascript/packs/users/index.tsx
@@ -7,41 +7,67 @@ const dataset = input.dataset;
 
 const data = {
   users_path: dataset.users_path
-}
+};
 
 const Index = () => {
-  const [userName, setUserName] = useState<string>()
-  const [userEmail, setEmail] = useState<string>()
-  const [userPassword, setUserPassword] = useState<string>()
-  const [userPasswordConfirm, setUserPasswordConfirm] = useState<string>()
+  const [userName, setUserName] = useState<string>();
+  const [userEmail, setEmail] = useState<string>();
+  const [userPassword, setUserPassword] = useState<string>();
+  const [userPasswordConfirm, setUserPasswordConfirm] = useState<string>();
 
   const userInfo = {
     userName: userName,
     userEmail: userEmail,
     userPassword: userPassword,
-    userPasswordConfirm: userPasswordConfirm,
-  }
+    userPasswordConfirm: userPasswordConfirm
+  };
 
-  return(
+  return (
     <div>
       <h1> Users Index </h1>
       <div>
         <label>名前</label>
-        <input name="name" placeholder="username" onBlur={e => {setUserName(e.target.value)}}></input>
+        <input
+          name="name"
+          placeholder="username"
+          onBlur={e => {
+            setUserName(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label>Email</label>
-        <input name="email" placeholder="email" onBlur={e => {setEmail(e.target.value)}}></input>
+        <input
+          name="email"
+          placeholder="email"
+          onBlur={e => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label>password</label>
-        <input type="password" name="password" onBlur={e => {setUserPassword(e.target.value)}}></input>
+        <input
+          type="password"
+          name="password"
+          onBlur={e => {
+            setUserPassword(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label>passwordの確認</label>
-        <input type="password" name="password_confirmation" onBlur={e => {setUserPasswordConfirm(e.target.value)}}></input>
+        <input
+          type="password"
+          name="password_confirmation"
+          onBlur={e => {
+            setUserPasswordConfirm(e.target.value);
+          }}
+        />
       </div>
-      <button type="submit" onClick={() => console.log(userInfo)}>決定</button>
+      <button type="submit" onClick={() => console.log(userInfo)}>
+        決定
+      </button>
     </div>
   );
 };
